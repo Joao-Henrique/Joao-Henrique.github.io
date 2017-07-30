c5eaa6b39927642a5313ade86e6007459ca06c43
Joao Henrique <jppulicidade@gamail.com>
2017-07-30 17:24:03 +0100

    test mail

--- js/contact_me.js
+++ js/contact_me.js
@@ -9,7 +9,7 @@ $(function() {
             // Prevent spam click and default submit behaviour
             $("#btnSubmit").attr("disabled", true);
             event.preventDefault();
-            
+            /*
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
@@ -20,7 +20,7 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
-            /*
+            
             $.ajax({
                 url: "././mail/contact_me.php",
                 type: "POST",
