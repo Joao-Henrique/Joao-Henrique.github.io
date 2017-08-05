36a5f1fa9d7d072539cbc0dce5c08a174c53fc01
Joao Henrique <jppulicidade@gamail.com>
2017-08-05 22:51:22 +0100

    finaly the tic tac toe project is working. Missing the text bellow . Finaly :)

--- js/jsPortfolio/tictactoePortfolio.js
+++ js/jsPortfolio/tictactoePortfolio.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+  //GAME LOGIC
   function playerTurn(turn, id) {
     var spotTaken = $("#" + id).text();
     if (spotTaken === "#") {
@@ -11,44 +12,44 @@ $(document).ready(function() {
         computerTurn();
         winCondition(turns, computersTurn);
       }
-    } 
+    }
   }
 
   //FUNCTION FOR PLAYER TURN
   function winCondition(turnArray, currentTurn) {
-    if (turnArray[0] === currentTurn && turnArray[1] === currentTurn && turnArray[2] === currentTurn) {
+    if (turnArray[10] === currentTurn && turnArray[11] === currentTurn && turnArray[12] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (Top row across 0,1, and 2 spots)");
-    } else if (turnArray[2] === currentTurn && turnArray[4] === currentTurn && turnArray[6] === currentTurn) {
+    } else if (turnArray[12] === currentTurn && turnArray[14] === currentTurn && turnArray[16] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (Top row across 2,4, and 6 spots)");
-    } else if (turnArray[0] === currentTurn && turnArray[3] === currentTurn && turnArray[6] === currentTurn) {
+    } else if (turnArray[10] === currentTurn && turnArray[13] === currentTurn && turnArray[16] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (1st row down 0,3, and 6 spots)");
-    } else if (turnArray[0] === currentTurn && turnArray[4] === currentTurn && turnArray[8] === currentTurn) {
+    } else if (turnArray[10] === currentTurn && turnArray[14] === currentTurn && turnArray[18] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (1st row diagonally across 0,4, and 8 spots)");
-    } else if (turnArray[1] === currentTurn && turnArray[4] === currentTurn && turnArray[7] === currentTurn) {
+    } else if (turnArray[11] === currentTurn && turnArray[14] === currentTurn && turnArray[17] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (2nd row down 1,4, and 7 spots)");
-    } else if (turnArray[2] === currentTurn && turnArray[5] === currentTurn && turnArray[8] === currentTurn) {
+    } else if (turnArray[12] === currentTurn && turnArray[15] === currentTurn && turnArray[18] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (3rd row down 2,5, and 8 spots)");
-    } else if (turnArray[2] === currentTurn && turnArray[5] === currentTurn && turnArray[8] === currentTurn) {
+    } else if (turnArray[12] === currentTurn && turnArray[15] === currentTurn && turnArray[18] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (3rd row across 2,4, and 6 spots)");
-    } else if (turnArray[3] === currentTurn && turnArray[4] === currentTurn && turnArray[5] === currentTurn) {
+    } else if (turnArray[13] === currentTurn && turnArray[14] === currentTurn && turnArray[15] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (Middle row across 3,4, and 5 spots)");
-    } else if (turnArray[6] === currentTurn && turnArray[7] === currentTurn && turnArray[8] === currentTurn) {
+    } else if (turnArray[16] === currentTurn && turnArray[17] === currentTurn && turnArray[18] === currentTurn) {
       gameOn = true;
       reset();
       alert("Player " + currentTurn + " wins! (Bottom row across 6,7, and 8 spots)");
@@ -60,8 +61,8 @@ $(document).ready(function() {
   //FUNCTION FOR COMPUTERS TURN
   function computerTurn() {
     var taken = false;
-    while (taken === false && count !== 5) {
-      var computersMove = (Math.random() * 10).toFixed();
+    while (taken === false && count !== 15) {
+      var computersMove = ((Math.random() * 10)+10).toFixed();
       var move = $("#" + computersMove).text();
       if (move === '#') {
         $('#' + computersMove).text(computersTurn);
@@ -71,32 +72,32 @@ $(document).ready(function() {
     }
   }
 
-  $(".tic").click(function() {
+  $(".ttttic").click(function() {
     var slot = $(this).attr('id');
     playerTurn(turn, slot);
   });
 
-  $("#turnX").click(function() {
+  $("#tttturnX").click(function() {
     reset();
     turn = 'X';
     computersTurn = 'O';
-    $("#turnO").removeClass("btn-primary");
-    $("#turnX").addClass("btn-primary");
+    $("#tttturnO").removeClass("btn-primary");
+    $("#tttturnX").addClass("btn-primary");
 
   });
-  $("#turnO").click(function() {
+  $("#tttturnO").click(function() {
     reset();
     turn = "O";
-    computersTurn = 'X'
-    $("#turnX").removeClass("btn-primary");
-    $("#turnO").addClass("btn-primary");
-    $(".tic").text("#");
+    computersTurn = 'X';
+    $("#tttturnX").removeClass("btn-primary");
+    $("#tttturnO").addClass("btn-primary");
+    $(".ttttic").text("#");
   });
 
   function reset() {
     turns = ["#", "#", "#", "#", "#", "#", "#", "#", "#"];
     count = 0;
-    $(".tic").text("#");
+    $(".ttttic").text("#");
     gameOn = false;
   }
 });
